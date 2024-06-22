4410cf6ff8723ac71d3824df03336c1f5919db78
Varun Kumar <varuntheace@gmail.com>
2024-06-22 18:18:34 +0530

    second

--- main.py
+++ main.py
@@ -1,34 +1,38 @@
 #i need to create a board
 def create_board(board):
-    for i in board:
-        for j in i:
-            print(j, end = ' ')
-        print('\n')
+    print(board[0] + '|' + board[1] + '|' + board[2])
+    print(board[3] + '|' + board[4] + '|' + board[5])
+    print(board[6] + '|' + board[7] + '|' + board[8])
+
 
 def return_board():
-    row = [['*'] * 3] * 3
+    row = ['-', '-', '-',
+           '-', '-', '-',
+           '-', '-', '-']
     return row
 
+def player_input(board):
+    player1_choice = input('O or X: ').lower()
+    if player1_choice != 'o' and player1_choice != 'x':
+        print('Please enter either o or x')
+        player_input(board)
+    player1_index = int(input("Enter a number from 1 to 9: "))
+    player1_index -= 1
+    board[player1_index] = player1_choice.upper()
+    # create_board(board)
+    # player2_choice = input('O or X: ').lower()
+    # player2_index = int(input("Enter a number from 1 to 9: "))
+    # player2_index -= 1
+    # board[player2_index] = player2_choice
+    create_board(board)
 def pvp():
     board = return_board()
     run = True
     while run:
-        create_board(board)
-        player1_choice = input('O or X: ').lower()
-        player1_index = int(input("Enter a number from 1 to 9: "))
-        player1_index += 1
-        if player1_index > 3:
-            row = player1_index % 3
-            col = player1_index - row
-            board[row][col] = player1_choice
-        create_board(board)
-        player2_choice = input('O or X: ').lower()
-        player2_index = int(input("Enter a number from 1 to 9: "))
-        player2_index += 1
-        if player2_index > 3:
-            row = player1_index % 3
-            col = player1_index - row - 1
-            board[row][col] = player2_choice
+        player_input(board)
+
+def win_or_lose():
+    
 def main():
     print("Welcome to Tic Tac Toe!")
     print("What mode would you like to play?:")
