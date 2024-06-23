ca439054cac3e966c63c8d7ee60dc28a73213383
Varun Kumar <varuntheace@gmail.com>
2024-06-23 12:11:23 +0530

    first try at ai

--- main.py
+++ main.py
@@ -76,6 +76,71 @@ def pvp():
             print("It's a tie!")
             break
 
+def is_winner(board, player):
+    win_condn = [[0, 1, 2], [3, 4, 5], [6, 7, 8],  # Horizontal wins
+                 [0, 3, 6], [1, 4, 7], [2, 5, 8],  # Vertical wins
+                 [0, 4, 8], [2, 4, 6]]
+    for i in win_condn:
+        if board[i[0]] == board[i[1]] == board[i[2]] == player:
+            return True
+    return False
+
+def is_draw(board):
+    return '-' not in board
+
+
+def minimax(board, depth, maximizing_player):
+    if is_winner(board, 'X'):
+        return 1
+    elif is_winner(board, 'O'):
+        return -1
+    elif is_draw(board):
+        return 0
+    if maximizing_player:
+        max_eval = -float('inf')
+        for i in range(len(board)):
+            if board[i] == '-':
+                board[i] = 'X'
+                eval = minimax(board, depth + 1, False)
+                board[i] = '-'
+                max_eval = max(max_eval, eval)
+        return max_eval
+    else:
+        min_eval = float('inf')
+        for i in range(len(board)):
+            if board[i] == '-':
+                board[i] = 'O'
+                eval = minimax(board, depth + 1, True)
+                board[i] = '-'
+                min_eval = min(min_eval, eval)
+        return min_eval
+def nextMove(board):
+    best_move = -1
+    best_score = -float('inf')
+    for i in range(len(board)):
+        if board[i] == '-':
+            board[i] = 'X'
+            score = minimax(board, 0, False)
+            board[i] = '-'
+            if score > best_score:
+                best_score = score
+                best_move = i
+    return best_move
+
+
+def pve():
+    board = return_board()
+    player_choice = input('Player-1, choose O or X: ').upper()
+    while player_choice not in ['O', 'X']:
+        print('Invalid choice! Please enter either O or X.')
+        player_choice = input('Player-1, choose O or X: ').upper()
+
+    p1 = player_choice
+    p2 = 'X' if p1 == 'O' else 'O'
+
+    current_player = 1
+    while True:
+        if current_player == 1
 
 def main():
     print("Welcome to Tic Tac Toe!")
