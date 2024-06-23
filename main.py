660665db6a2bf1140dc072ce3271ed8693793a1e
Varun Kumar <varuntheace@gmail.com>
2024-06-23 09:03:08 +0530

    tic tac toe over ai to be made
    finished pvp modee!!

--- main.py
+++ main.py
@@ -13,64 +13,68 @@ def return_board():
 
 
 def player_input(board, player_choice):
-    player_index = int(input("Enter a number from 1 to 9: "))
-    if board[player_index] == player_choice:
-        print(f"{player_choice} is already marked!")
-        player_input(board, player_choice)
-    player_index -= 1
-    win = False
-    win_condn = []
-    for i in range(0, 9):
-        if i == player_index:
-            if i % 3 == 0:
-                if board[i+1] == player_choice and board[i+2] == player_choice:
-                    win = True
-                elif board[i+3]
-    if player_index == 0:
-        if board[1] == player_choice and board[2] == player_choice:
-            win = True
-        elif board[4] == player_choice and board[8] == player_choice:
-            win = True
-        elif board[3] == player_choice and board[6] == player_choice:
-            win = True
-    elif player_index == 1:
-        if board[0] == player_choice and board[2] == player_choice:
-            win = True
-        elif board[4] == player_choice and board[7] == player_choice:
-            win = True
-    elif player_index == 2:
-        if board[1] == player_choice and board[0] == player_choice:
-            win = True
-        elif board[5] == player_choice and board[8] == player_choice:
-            win = True
-        elif board[4] == player_choice and board[6] == player_choice:
-            win = True
+    while True:
+        try:
+            player_index = int(input(f"Enter a number from 1 to 9 to place your {player_choice}: ")) - 1
+            if player_index < 0 or player_index > 8:
+                print("Invalid input! Please enter a number from 1 to 9.")
+                continue
+            if board[player_index] != '-':
+                print(f"Position {player_index + 1} is already marked!")
+                continue
+            break
+        except ValueError:
+            print("Invalid input! Please enter a valid number from 1 to 9.")
     board[player_index] = player_choice.upper()
     create_board(board)
+    return player_index
+
+
+def win_or_lose(board, player_choice, player_index):
+    win = False
+    win_condn = [[0, 1, 2], [3, 4, 5], [6, 7, 8],  # Horizontal wins
+                 [0, 3, 6], [1, 4, 7], [2, 5, 8],  # Vertical wins
+                 [0, 4, 8], [2, 4, 6]]
+    for i in win_condn:
+        if player_index in i:
+            if board[i[0]] == board[i[1]] == board[i[2]] == player_choice.upper():
+                win = True
+                break
+
+    return win
+
 
 
 def pvp():
     board = return_board()
-    run = True
-    while run:
-        player_turn(board)
+    player_choice = input('Player-1, choose O or X: ').upper()
+    while player_choice not in ['O', 'X']:
+        print('Invalid choice! Please enter either O or X.')
+        player_choice = input('Player-1, choose O or X: ').upper()
 
-
-def player_turn(board):
-    player_choice = input('O or X: ').lower()
-    if player_choice != 'o' and player_choice != 'x':
-        print('Please enter either o or x')
-        board = return_board()
-        player_turn(board)
     p1 = player_choice
-    if player_choice == 'o':
-        p2 = 'X'
-    else:
-        p2 = 'O'
-    print("Player-1")
-    player_input(board, p1)
-    print("Player-2")
-    player_input(board, p2)
+    p2 = 'X' if p1 == 'O' else 'O'
+
+    current_player = 1
+    while True:
+        if current_player == 1:
+            print("Player-1's turn")
+            player_index = player_input(board, p1)
+            if win_or_lose(board, p1, player_index):
+                print("Player-1 wins!")
+                break
+            current_player = 2
+        else:
+            print("Player-2's turn")
+            player_index = player_input(board, p2)
+            if win_or_lose(board, p2, player_index):
+                print("Player-2 wins!")
+                break
+            current_player = 1
+
+        if '-' not in board:
+            print("It's a tie!")
+            break
 
 
 def main():
